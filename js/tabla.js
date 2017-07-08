25e24ade55c2dbfd25047412c0a0f2f2fa68e2ed
dajocape <danijo1995@gmail.com>
2017-07-08 18:09:05 -0500

    Pruebas de inea de tiempo

--- js/tabla.js
+++ js/tabla.js
@@ -1,47 +1,59 @@
 ﻿function llenarTabla(){
 	$.ajax({
-		url:"json/datos.json",
-		data:{format:"json"},
-		error:function(){window.alert("No se pudo cargar el json");},
-		dataType:"jsonp",
+		url:'json/datos.json',
+		data:{format:'json'},
+		error:function(){window.alert('No se pudo cargar el json');},
+		dataType:'jsonp',
 		success:function(data){
-			var $fila, $fecha, $objeto, $lugar, $link, $cont;
+			var $fila, $cabecera, $elemento, $fecha, $objeto, $lugar, $link, $cont;
+			$cabecera = $('<thead>');
+			$fila = $('<tr>');
+			$elemento = $('<td>').text('Fecha');
+			$fila.append($elemento);
+			$elemento = $('<td>').text('Objeto');
+			$fila.append($elemento);
+			$elemento = $('<td>').text('Lugar');
+			$fila.append($elemento);
+			$elemento = $('<td>').text('Detalles');
+			$fila.append($elemento);
+			$cabecera.append($fila);
+			$('#tabla').append($cabecera);
 			$.each(data, function(){
-				$fila = $("<tr>");
-				$fecha = $("<td>").text(data.fecha);
+				$fila = $('<tr>');
+				$fecha = $('<td>').text(data.fecha);
 				if(data.objeto.lenght > 1){
 					for(var i = 0; i < data.objeto.lenght; i++){
-						$objeto = $("<td>").text(data.objeto[i]);
-						$lugar = $("<td>").text(data.lugar[i]);
-						$cont = $("<td>");
-						$link = $("<a>");
-						$link.text("Ver detalles");
-						$link.setAttribute("href",data.link[i]);
+						$objeto = $('<td>').text(data.objeto[i]);
+						$lugar = $('<td>').text(data.lugar[i]);
+						$cont = $('<td>');
+						$link = $('<a>');
+						$link.text('Ver detalles');
+						$link.setAttribute('href',data.link[i]);
 						$cont.append($link);
 						$fila.append($fecha);
 						$fila.append($objeto);
 						$fila.append($lugar);
 						$fila.append($cont);
-						$("#tabla").append($fila);
+						$('#tabla').append($fila);
 					}
 				} else {
-					$objeto = $("<td>").text(data.objeto[0]);
-					$lugar = $("<td>").text(data.lugar[0]);
-					$cont = $("<td>");
-					$link = $("<a>");
-					$link.text("Ver detalles");
-					$link.setAttribute("href",data.link[0]);
+					$objeto = $('<td>').text(data.objeto[0]);
+					$lugar = $('<td>').text(data.lugar[0]);
+					$cont = $('<td>');
+					$link = $('<a>');
+					$link.text('Ver detalles');
+					$link.setAttribute('href',data.link[0]);
 					$cont.append($link);
 					$fila.append($fecha);
 					$fila.append($objeto);
 					$fila.append($lugar);
 					$fila.append($cont);
-					$("#tabla").append($fila);
+					$('#tabla').append($fila);
 				}
 			});
 		},
-		tyoe:"GET"
-	})
+		type:'GET'
+	});
 }
 
 window.onload = function(){
