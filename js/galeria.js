f6f666906b1b937b7327008a707f7a94202a2e27
dajocape <danijo1995@gmail.com>
2017-07-08 14:05:37 -0500

    Ajax

---
+++ js/galeria.js
@@ -0,0 +1,13 @@
+﻿var ancho, lista, estilo;
+function establecerAltura(){
+	lista = document.getElementsByClassName("imagen");
+	for(var i = 0; i < lista.length; i++){
+		estilo = window.getComputedStyle(lista[i]);
+		ancho = estilo.getPropertyValue("width");
+		lista[i].style.heigth = ancho;
+		window.alert("elemento: "+i+"altura: " + lista[i].style.heigth+ "ancho: "+ancho);
+	}
+}
+window.onload = function() {
+	establecerAltura();
+};
---
+++ js/tabla.js
@@ -0,0 +1,49 @@
+﻿function llenarTabla(){
+	$.ajax({
+		url:"json/datos.json",
+		data:{format:"json"},
+		error:function(){window.alert("No se pudo cargar el json");},
+		dataType:"jsonp",
+		success:function(data){
+			var $fila, $fecha, $objeto, $lugar, $link, $cont;
+			$.each(data, function(){
+				$fila = $("<tr>");
+				$fecha = $("<td>").text(data.fecha);
+				if(data.objeto.lenght > 1){
+					for(var i = 0; i < data.objeto.lenght; i++){
+						$objeto = $("<td>").text(data.objeto[i]);
+						$lugar = $("<td>").text(data.lugar[i]);
+						$cont = $("<td>");
+						$link = $("<a>");
+						$link.text("Ver detalles");
+						$link.setAttribute("href",data.link[i]);
+						$cont.append($link);
+						$fila.append($fecha);
+						$fila.append($objeto);
+						$fila.append($lugar);
+						$fila.append($cont);
+						$("#tabla").append($fila);
+					}
+				} else {
+					$objeto = $("<td>").text(data.objeto[0]);
+					$lugar = $("<td>").text(data.lugar[0]);
+					$cont = $("<td>");
+					$link = $("<a>");
+					$link.text("Ver detalles");
+					$link.setAttribute("href",data.link[0]);
+					$cont.append($link);
+					$fila.append($fecha);
+					$fila.append($objeto);
+					$fila.append($lugar);
+					$fila.append($cont);
+					$("#tabla").append($fila);
+				}
+			});
+		},
+		tyoe:"GET"
+	})
+}
+
+window.onload = function(){
+	llenarTabla();
+}
