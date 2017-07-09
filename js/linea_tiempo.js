233316baac954e209374bc4d6c8aea427b186b96
dajocape <danijo1995@gmail.com>
2017-07-08 23:42:49 -0500

    C

--- js/linea_tiempo.js
+++ js/linea_tiempo.js
@@ -8,7 +8,7 @@ function supportsSvg() {
         );
 }
 
-function getDataFromDefinitionList(linea) { /*ESTO NO SERIA NECESARIO SI USAMOS LOS DATOS QUE SE USARON PARA LA TABLA*/
+function getDataFromDefinitionList(linea) { 
   	var children = linea.children;
   	var yearIndex = {};
   	var data = [];
@@ -73,11 +73,62 @@ function drawTimeline(svgElement, data) {
   	});
 }
 
-if (supportsSvg()) {
-  	var linea = document.querySelector('#linea');
-  	linea.style.display = 'none';
-  	var data = getDataFromDefinitionList(linea);
-  	var svgElement = createSvgElement();
-  	linea.parentNode.insertBefore(svgElement, linea);
-  	drawTimeline(svgElement, data);
+function cargarLineaTiempo(){
+	if (supportsSvg()) {
+	  	var linea = document.querySelector('#linea');
+	  	linea.style.display = 'none';
+	  	var data = getDataFromDefinitionList(linea);
+	  	var svgElement = createSvgElement();
+	  	linea.parentNode.insertBefore(svgElement, linea);
+	  	drawTimeline(svgElement, data);
+	}
+}
+
+function llenarTabla(){
+	$.ajax({
+		url:'json/datos.json',
+		data:{format:'Json'},
+		error:function(){
+	        alert("No se pudo cargar el archivo JSON");
+	    },
+		dataType:'Json',
+		success:function(data){
+			var $fila, $cabecera, $elemento, $fecha, $objeto, $lugar, $link, $cont;
+			$cabecera = $('<thead>');
+			$fila = $('<tr>');
+			$elemento = $('<th>').text('Fecha');
+			$fila.append($elemento);
+			$elemento = $('<th>').text('Objeto');
+			$fila.append($elemento);
+			$elemento = $('<th>').text('Lugar');
+			$fila.append($elemento);
+			$elemento = $('<th>').text('Detalles');
+			$fila.append($elemento);
+			$cabecera.append($fila);
+			$('#tabla').append($cabecera);
+			$.each(data, function(){
+				for(var i = 0; i < this.objeto.length; i++){
+					$fila = $('<tr>');
+					$fecha = $('<td>').text(this.fecha);
+					$objeto = $('<td>').text(this.objeto[i]);
+					$lugar = $('<td>').text(this.lugar[i]);
+					$cont = $('<td>');
+					$link = $('<a>');
+					$link.text('Ver detalles');
+					$link.attr('href',this.link[i]);
+					$cont.append($link);
+					$fila.append($fecha);
+					$fila.append($objeto);
+					$fila.append($lugar);
+					$fila.append($cont);
+					$('#tabla').append($fila);
+				}
+			});
+		},
+		type:'GET',
+	});
+}
+
+window.onload = function(){
+	llenarTabla();
 }
